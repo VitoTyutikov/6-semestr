98effd0e8ebe324b2e44512c939cd7c46f738c40
vitaly <dbnf11dbnf@gmail.com>
2023-02-21 01:23:13 +0300

    Task 1

---
+++ parallel_computing/omp/hello_world_with_8_threads.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "omp.h"
+int main()
+{
+    omp_set_num_threads(8);
+#pragma omp parallel default(none)
+    {
+        printf("num_thread = %d\t threads_num = %d\t Hello, World!\n", omp_get_thread_num(),
+               omp_get_num_threads());
+    }
+    return 0;
+}
