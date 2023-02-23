2ab77bec2768c8918237eaa21aadfba91bc0541c
VitoCoder <dbnf11dbnf@gmail.com>
2023-02-23 16:29:09 +0300

    Task 2

--- parallel_computing/omp/src/test.cpp
+++ parallel_computing/omp/src/test.cpp
@@ -7,13 +7,15 @@ int main() {
 #pragma omp parallel if(num_threads > 2) num_threads(num_threads) default(none) shared(num_threads)
     {
         int thread_id = omp_get_thread_num();
-        printf("Thread %d of %d in first parallel region\n", thread_id, num_threads);
+        if(num_threads > 2)
+            printf("Thread %d of %d in first parallel region\n", thread_id, num_threads);
     }
     num_threads = 2;
 #pragma omp parallel if(num_threads > 2) num_threads(num_threads) default(none) shared(num_threads)
     {
         int thread_id = omp_get_thread_num();
-        printf("Thread %d of %d in second parallel region\n", thread_id, num_threads);
+        if(num_threads > 2)
+            printf("Thread %d of %d in second parallel region\n", thread_id, num_threads);
     }
 
     return 0;
